9df6d43d866a71ab1a542dc6a7d6ae18aa8678eb
angelg23 <angel.ortiz49@upr.edu>
2022-01-28 16:13:33 -0400

    Added Dual Creation script

--- Dual_Dataset_Creation.py
+++ Dual_Dataset_Creation.py
@@ -1,7 +1,8 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import os
-
+import json
+from datetime import date
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands = 1)
@@ -9,6 +10,12 @@ predictions = []
 padding = 50
 alphabet = 'abcdefghijklmnopqrstuvwxyz-;'
 classification_directory = os.getcwd() + '/Classification_Dataset'
+obj_directory = os.getcwd() + '/Obj_Dataset'
+f = open(obj_directory + '/Annotations.json')
+Annotations = json.load(f)
+alphabet_dict = {}
+for i, c in enumerate(alphabet):
+    alphabet_dict.setdefault(c, i)
 
 def SaveImageClassification(key, hand):
     letter = chr(key)
@@ -26,7 +33,38 @@ def SaveImageClassification(key, hand):
 
 
 def SaveImageObjDetect(key, hand, bbox):
-    return
+    letter = chr(key)
+    if letter == '-':
+        letter = "Space"
+    if letter == ';':
+        letter = 'Delete'
+
+    print("Saving to Obj detection dataset...")
+    if os.getcwd() != obj_directory + '/Data':
+        os.chdir(obj_directory + '/Data')
+    count = len([name for name in os.listdir('.') if os.path.isfile(name)])
+    (h, w)  = hand.shape[:2]
+    img_dict = {
+        "license" : 1,
+        "filename" : f'{count}.jpg',
+        "width": w,
+        "height": h,
+        "id": count
+    }
+    print("     Adding image to data folder")
+    Annotations['images'].append(img_dict)
+
+    annotation = {
+        "image_id" : count,
+        "id" : len(Annotations['annotations']),
+        "bbox" : bbox,
+        "category_id" : alphabet_dict[letter]
+
+    }
+    print("     Adding annotation...")
+    Annotations['annotations'].append(annotation)
+
+
 
 while True:
     succ, img = cap.read()
@@ -50,20 +88,16 @@ while True:
         cv2.imshow("Hand", hand)
         key = cv2.waitKey(1) & 0xFF
         if chr(key) in alphabet:
+            SaveImageObjDetect(key, hand, bbox)
             SaveImageClassification(key, hand)
 
-
-        # if key == ord('a'):
-        #     print(os.getcwd())
-        #     if os.getcwd() != directory:
-        #         os.chdir(directory)
-        #
-        #     count = len([name for name in os.listdir('.') if os.path.isfile(name)])
-        #     filename = f'a_{count}.jpg'
-        #     cv2.imwrite(filename, hand)
 
     cv2.imshow('MainWin', img)
     close = cv2.waitKey(1) & 0xFF
 
     if close == ord("."):
+        json_file = json.dumps(Annotations, indent=4)
+        os.chdir(obj_directory)
+        with open("Annotations.json", "w") as outfile:
+            outfile.write(json_file)
         break
